7615375a035bfc12f5730033761cc6cc0a5f4954
Cauã Franco <franco.cauaa@gmail.com>
2026-04-07 23:34:52 -0300

    Implementação do exercício 3

---
+++ ListaSimples.java
@@ -0,0 +1,31 @@
+public class ListaSimples {
+    private Node head;
+
+    public ListaSimples(){
+        this.head = null;
+    }
+
+    public void insertFirst(Object item){
+        Node novoNo = new Node(item, head);
+        head = novoNo;
+    }
+
+    public void imprimir(){
+        Node atual = head;
+
+        while(atual != null){
+            System.out.print(atual.item + " -> ");
+            atual = atual.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListaSimples lista = new ListaSimples();
+        lista.insertFirst("C");
+        lista.insertFirst("B");
+        lista.insertFirst("A");
+        lista.imprimir();
+    }
+}
